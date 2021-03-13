835a6765d05235483601a87eeeead2d51245398b
Musab Guma'a <musabgumaa@gmail.com>
2021-03-13 23:09:08 +0000

    Updated url config.

--- src/helpers/httpconfig.ts
+++ src/helpers/httpconfig.ts
@@ -1,5 +1,5 @@
 const Config = {
-    backend_base_url: "http://localhost:3000"
+    backend_base_url: (() => (!process.env.NODE_ENV || process.env.NODE_ENV === 'development') ? "http://localhost:3001" : "https://api.colandria.com:3001")()
 };
 
 export default Config;
