7b1eb2584e2b06ecb006439eef7cbdfc097c353c
Musab Guma'a <musabgumaa@gmail.com>
2021-04-03 10:39:30 +0100

    Updated backend endpoint config & build script.

--- src/helpers/httpconfig.ts
+++ src/helpers/httpconfig.ts
@@ -1,5 +1,5 @@
 const Config = {
-    backend_base_url: (() => (!process.env.NODE_ENV || process.env.NODE_ENV === 'development') ? "http://localhost:3001" : "https://api.colandria.com:3001")()
+    backend_base_url: (() => (!process.env.NODE_ENV || process.env.NODE_ENV === 'development') ? "http://localhost:3001" : "https://colandria-api.fly.dev")()
 };
 
 export default Config;
